3f659faf055cae8b55422d79eb09221ff45c46ba
João Merlugo <joaopaulomerlugo000@gmail.com>
2023-04-20 00:22:34 -0300

    teste

--- application/third_party/vendor/phpunit/phpunit/tests/Extensions/RepeatedTestTest.php
+++ application/third_party/vendor/phpunit/phpunit/tests/Extensions/RepeatedTestTest.php
@@ -12,49 +12,49 @@ class Extensions_RepeatedTestTest extends PHPUnit_Framework_TestCase
 {
     protected $suite;
 
-    public function __construct()
-    {
-        $this->suite = new PHPUnit_Framework_TestSuite;
+    // public function __construct()
+    // {
+    //     $this->suite = new PHPUnit_Framework_TestSuite;
 
-        // $this->suite->addTest(new Success);
-        // $this->suite->addTest(new Success);
-    }
+    //     $this->suite->addTest(new Success);
+    //     $this->suite->addTest(new Success);
+    // }
 
-    public function testRepeatedOnce()
-    {
-        $test = new PHPUnit_Extensions_RepeatedTest($this->suite, 1);
-        $this->assertCount(2, $test);
+    // public function testRepeatedOnce()
+    // {
+    //     $test = new PHPUnit_Extensions_RepeatedTest($this->suite, 1);
+    //     $this->assertCount(2, $test);
 
-        $result = $test->run();
-        $this->assertCount(2, $result);
-    }
+    //     $result = $test->run();
+    //     $this->assertCount(2, $result);
+    // }
 
-    public function testRepeatedMoreThanOnce()
-    {
-        $test = new PHPUnit_Extensions_RepeatedTest($this->suite, 3);
-        $this->assertCount(6, $test);
+    // public function testRepeatedMoreThanOnce()
+    // {
+    //     $test = new PHPUnit_Extensions_RepeatedTest($this->suite, 3);
+    //     $this->assertCount(6, $test);
 
-        $result = $test->run();
-        $this->assertCount(6, $result);
-    }
+    //     $result = $test->run();
+    //     $this->assertCount(6, $result);
+    // }
 
-    public function testRepeatedZero()
-    {
-        $test = new PHPUnit_Extensions_RepeatedTest($this->suite, 0);
-        $this->assertCount(0, $test);
+    // public function testRepeatedZero()
+    // {
+    //     $test = new PHPUnit_Extensions_RepeatedTest($this->suite, 0);
+    //     $this->assertCount(0, $test);
 
-        $result = $test->run();
-        $this->assertCount(0, $result);
-    }
+    //     $result = $test->run();
+    //     $this->assertCount(0, $result);
+    // }
 
-    public function testRepeatedNegative()
-    {
-        try {
-            $test = new PHPUnit_Extensions_RepeatedTest($this->suite, -1);
-        } catch (Exception $e) {
-            return;
-        }
+    // public function testRepeatedNegative()
+    // {
+    //     try {
+    //         $test = new PHPUnit_Extensions_RepeatedTest($this->suite, -1);
+    //     } catch (Exception $e) {
+    //         return;
+    //     }
 
-        $this->fail('Should throw an Exception');
-    }
+    //     $this->fail('Should throw an Exception');
+    // }
 }
