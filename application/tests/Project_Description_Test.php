7480c24361656573b0f600721cd5dbffb767227f
João Merlugo <joaopaulomerlugo000@gmail.com>
2023-04-25 10:50:12 -0300

    test 2

--- application/tests/Project_Description_Test.php
+++ application/tests/Project_Description_Test.php
@@ -1,6 +1,6 @@
 <?php
-use PHPUnit\Framework\TestCase;
-include_once "../models/Project_Model.php";
+// use PHPUnit\Framework\TestCase;
+// include_once "../models/Project_Model.php";
 
 class ProjectTest extends TestCase
 {
